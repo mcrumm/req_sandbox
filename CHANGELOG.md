50973b79ad6649cff58fb61b47daa02e9b4009ba
Michael Crumm <michael.crumm@cargosense.com>
2022-12-13 14:20:48 -0800

    Release v0.1.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # CHANGELOG
 
+## v0.1.1 (2022-12-13)
+
+- Fixes a regression when using Req's `:base_url` option.
+
 ## v0.1.0 (2022-10-25)
 
 Initial release
--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule ReqSandbox.MixProject do
 
   @source_url "https://github.com/mcrumm/req_sandbox"
 
-  @version "0.1.0"
+  @version "0.1.1"
 
   def project do
     [
