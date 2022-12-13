1334f11b85d8fdadfa050490ccf27c530a52f9dc
Michael Crumm <michael.crumm@cargosense.com>
2022-12-13 11:53:00 -0800

    Add base_url test

--- test/req_sandbox_test.exs
+++ test/req_sandbox_test.exs
@@ -3,50 +3,58 @@ defmodule ReqSandboxTest do
   doctest ReqSandbox
   alias Plug.Conn
 
+  def plug(%Conn{method: "POST", path_info: path} = conn, test_pid)
+      when path in [["sandbox"], ["sandbox-custom"]] do
+    encoded = %{ref: make_ref()} |> :erlang.term_to_binary() |> Base.url_encode64()
+    sandbox = "FakeSandbox (#{encoded})"
+
+    url =
+      "#{conn.scheme}://#{conn.host}:#{conn.port}#{conn.request_path}"
+      |> URI.parse()
+      |> URI.to_string()
+
+    send(test_pid, {:sandbox_called, url, sandbox})
+
+    conn
+    |> Plug.Conn.put_resp_content_type("text/plain")
+    |> Plug.Conn.send_resp(200, sandbox)
+  end
+
+  def plug(%Conn{method: "DELETE", path_info: path} = conn, test_pid)
+      when path in [["sandbox"], ["sandbox-custom"]] do
+    header = Map.get(conn.query_params, "header", "user-agent")
+    [sandbox] = Plug.Conn.get_req_header(conn, header)
+
+    send(test_pid, {:sandbox_deleted, sandbox})
+
+    conn
+    |> Plug.Conn.put_resp_content_type("text/plain")
+    |> Plug.Conn.send_resp(200, "")
+  end
+
+  def plug(%Conn{path_info: ["headers", header]} = conn, _test_pid) do
+    [value] = Plug.Conn.get_req_header(conn, header)
+
+    conn
+    |> Plug.Conn.put_resp_content_type("text/plain")
+    |> Plug.Conn.send_resp(200, value)
+  end
+
+  def plug(%Conn{} = conn, _test_pid) do
+    raise ArgumentError, "invalid path, got: #{inspect(conn.request_path)}"
+  end
+
   setup do
     test_pid = self()
 
-    plug = fn
-      %Conn{method: "POST", path_info: path} = conn
-      when path in [["sandbox"], ["sandbox-custom"]] ->
-        encoded = %{ref: make_ref()} |> :erlang.term_to_binary() |> Base.url_encode64()
-        sandbox = "FakeSandbox (#{encoded})"
-        send(test_pid, {:sandbox_called, sandbox})
-
-        conn
-        |> Plug.Conn.put_resp_content_type("text/plain")
-        |> Plug.Conn.send_resp(200, sandbox)
-
-      %Conn{method: "DELETE", path_info: path} = conn
-      when path in [["sandbox"], ["sandbox-custom"]] ->
-        header = Map.get(conn.query_params, "header", "user-agent")
-        [sandbox] = Plug.Conn.get_req_header(conn, header)
-
-        send(test_pid, {:sandbox_deleted, sandbox})
-
-        conn
-        |> Plug.Conn.put_resp_content_type("text/plain")
-        |> Plug.Conn.send_resp(200, "")
-
-      %Conn{path_info: ["headers", header]} = conn ->
-        [value] = Plug.Conn.get_req_header(conn, header)
-
-        conn
-        |> Plug.Conn.put_resp_content_type("text/plain")
-        |> Plug.Conn.send_resp(200, value)
-
-      %Conn{} = conn ->
-        raise ArgumentError, "invalid path, got: #{inspect(conn.request_path)}"
-    end
-
-    %{req: Req.new(plug: plug)}
+    %{req: Req.new(plug: &plug(&1, test_pid))}
   end
 
   test "requests without a sandbox create a new sandbox", %{req: req} do
     req = req |> ReqSandbox.attach()
     res = req |> Req.get!(url: "/headers/user-agent")
 
-    assert_received {:sandbox_called, encoded}
+    assert_received {:sandbox_called, _, encoded}
     assert Process.get(:req_sandbox) == encoded
 
     assert res.body == encoded
@@ -56,12 +64,12 @@ defmodule ReqSandboxTest do
     req = req |> ReqSandbox.attach()
     res = req |> Req.get!(url: "/headers/user-agent")
 
-    assert_received {:sandbox_called, encoded}
+    assert_received {:sandbox_called, _, encoded}
     assert res.body == encoded
 
     res2 = req |> Req.get!(url: "/headers/user-agent")
     assert res2.body == encoded
-    refute_received {:sandbox_called, ^encoded}
+    refute_received {:sandbox_called, _, ^encoded}
   end
 
   test "delete!/0 deletes the sandbox token from the process dictionary", %{req: req} do
@@ -70,14 +78,14 @@ defmodule ReqSandboxTest do
     req = req |> ReqSandbox.attach()
     res = req |> Req.get!(url: "/headers/user-agent")
 
-    assert_received {:sandbox_called, encoded}
+    assert_received {:sandbox_called, _, encoded}
     assert res.body == encoded
 
     assert ReqSandbox.delete!() == encoded
 
     res = req |> Req.get!(url: "/headers/user-agent")
 
-    assert_received {:sandbox_called, encoded2}
+    assert_received {:sandbox_called, _, encoded2}
     assert res.body == encoded2
 
     assert encoded2 != encoded
@@ -88,7 +96,7 @@ defmodule ReqSandboxTest do
 
     req |> Req.get!(url: "/headers/user-agent")
 
-    assert_received {:sandbox_called, encoded}
+    assert_received {:sandbox_called, _, encoded}
 
     assert ReqSandbox.delete!(req) == encoded
 
@@ -99,7 +107,7 @@ defmodule ReqSandboxTest do
     req = req |> ReqSandbox.attach(sandbox_url: "/sandbox-custom")
     res = req |> Req.get!(url: "/headers/user-agent")
 
-    assert_received {:sandbox_called, encoded}
+    assert_received {:sandbox_called, _, encoded}
     assert res.body == encoded
   end
 
@@ -107,7 +115,7 @@ defmodule ReqSandboxTest do
     req = req |> ReqSandbox.attach(sandbox_header: :x_phoenix_ecto_sandbox)
     res = req |> Req.get!(url: "/headers/x-phoenix-ecto-sandbox")
 
-    assert_received {:sandbox_called, encoded}
+    assert_received {:sandbox_called, _, encoded}
     assert res.body == encoded
 
     ReqSandbox.delete!()
@@ -115,7 +123,7 @@ defmodule ReqSandboxTest do
     req = req |> ReqSandbox.attach(sandbox_header: "x-phoenix-ecto-sandbox")
     res = req |> Req.get!(url: "/headers/x-phoenix-ecto-sandbox")
 
-    assert_received {:sandbox_called, encoded}
+    assert_received {:sandbox_called, _, encoded}
     assert res.body == encoded
   end
 
@@ -123,7 +131,7 @@ defmodule ReqSandboxTest do
     req = req |> ReqSandbox.attach(sandbox_url: "/sandbox-custom")
     res = req |> Req.get!(url: "/headers/user-agent")
 
-    assert_received {:sandbox_called, encoded}
+    assert_received {:sandbox_called, _, encoded}
     assert res.body == encoded
 
     res2 =
@@ -136,5 +144,16 @@ defmodule ReqSandboxTest do
       |> Task.await()
 
     assert res2.body == encoded
+  end
+
+  test "with base_url" do
+    test_pid = self()
+
+    _ =
+      Req.new(base_url: "http://req-sandbox.example", plug: &plug(&1, test_pid))
+      |> ReqSandbox.attach()
+      |> Req.get!(url: "/headers/user-agent")
+
+    assert_received {:sandbox_called, "http://req-sandbox.example/sandbox", _}
   end
 end
